6f28b74f37cb35b9a789191dd96ddd14443616a3
SaxatileStoner <christopherstoner0326@gmail.com>
2022-04-23 22:29:40 -0700

    Initial Commit

---
+++ lab6.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', current_date_time=datetime.today().ctime())
+
+
+@app.route('/games-list')
+def games_list():
+    return render_template('games-list.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
