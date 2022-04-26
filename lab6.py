dd2bb1ee00b9a7db15d10e83e79ac294075811a0
SaxatileStoner <christopherstoner0326@gmail.com>
2022-04-25 18:01:49 -0700

    Added doc comment to all functions

--- lab6.py
+++ lab6.py
@@ -6,23 +6,34 @@ TODO Grammer and spelling checks for html files
 TODO Documentation document
 """
 from datetime import datetime
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', current_date_time=datetime.today().ctime())
+    """
+    Renders index.html and gives date and time as a variable
+    for index.html to use. Allows user to see time and date of access.
+    """
+    return render_template('index.html',
+                           current_date_time=datetime.today().ctime())
 
 
 @app.route('/games-list')
 def games_list():
+    """
+    Renders games-list.html web page
+    """
     return render_template('games-list.html')
 
 
 @app.route('/photogrpahy')
 def photography():
+    """
+    Renders photography.html web page
+    """
     return render_template('photography.html')
 
 
